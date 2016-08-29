3c5bb4b52d73e2ef01b9cb4923d91921bf734ade
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-08-29 11:48:20 +0530

    Use mysql syntax

--- assignment_3/university/schema.sql
+++ assignment_3/university/schema.sql
@@ -1,12 +1,12 @@
 -- University DDL statements
-create table classroom (
+create table if not exists classroom (
 	building    varchar(15),
 	room_number varchar(7),
 	capacity    numeric(4,0),
 	primary key (building, room_number)
 );
 
-create table department (
+create table if not exists department (
 	dept_name   varchar(20),
 	building    varchar(15),
 	budget      numeric(12,2)
@@ -14,29 +14,29 @@ create table department (
 	primary key (dept_name)
 );
 
-create table course (
+create table if not exists course (
 	course_id   varchar(8),
 	title       varchar(50),
 	dept_name   varchar(20),
 	credits     numeric(2,0)
 		check (credits > 0),
 	primary key (course_id),
-	foreign key (dept_name) references department
+	foreign key (dept_name) references department(dept_name)
 		on delete set null
 );
 
-create table instructor (
+create table if not exists instructor (
 	ID          varchar(5),
 	name        varchar(20) not null,
 	dept_name   varchar(20),
 	salary      numeric(8,2)
 		check (salary > 29000),
 	primary key (ID),
-	foreign key (dept_name) references department
+	foreign key (dept_name) references department(dept_name)
 		on delete set null
 );
 
-create table section (
+create table if not exists section (
 	course_id    varchar(8),
 	sec_id       varchar(8),
 	semester     varchar(6)
@@ -47,37 +47,37 @@ create table section (
 	room_number  varchar(7),
 	time_slot_id varchar(4),
 	primary key  (course_id, sec_id, semester, year),
-	foreign key  (course_id) references course
+	foreign key  (course_id) references course(course_id)
 		on delete cascade,
-	foreign key  (building, room_number) references classroom
+	foreign key  (building, room_number) references classroom(building, room_number)
 		on delete set null
 );
 
-create table teaches (
+create table if not exists teaches (
 	ID			varchar(5),
 	course_id	varchar(8),
 	sec_id		varchar(8),
 	semester	varchar(6),
 	year		numeric(4,0),
 	primary key (ID, course_id, sec_id, semester, year),
-	foreign key (course_id,sec_id, semester, year) references section
+	foreign key (course_id,sec_id, semester, year) references section(course_id, sec_id, semester, year)
 		on delete cascade,
-	foreign key (ID) references instructor
+	foreign key (ID) references instructor(ID)
 		on delete cascade
 );
 
-create table student (
+create table if not exists student (
 	ID          varchar(5),
 	name        varchar(20) not null,
 	dept_name   varchar(20),
 	tot_cred    numeric(3,0)
 		check (tot_cred >= 0),
 	primary key (ID),
-	foreign key (dept_name) references department
+	foreign key (dept_name) references department(dept_name)
 		on delete set null
 );
 
-create table takes (
+create table if not exists takes (
 	ID          varchar(5),
 	course_id   varchar(8),
 	sec_id      varchar(8),
@@ -85,23 +85,23 @@ create table takes (
 	year        numeric(4,0),
 	grade       varchar(2),
 	primary key (ID, course_id, sec_id, semester, year),
-	foreign key (course_id,sec_id, semester, year) references section
+	foreign key (course_id,sec_id, semester, year) references section(course_id, sec_id, semester, year)
 		on delete cascade,
-	foreign key (ID) references student
+	foreign key (ID) references student(ID)
 		on delete cascade
 );
 
-create table advisor (
+create table if not exists advisor (
 	s_ID        varchar(5),
 	i_ID        varchar(5),
 	primary key (s_ID),
-	foreign key (i_ID) references instructor (ID)
+	foreign key (i_ID) references instructor(ID)
 		on delete set null,
-	foreign key (s_ID) references student (ID)
+	foreign key (s_ID) references student(ID)
 		on delete cascade
 );
 
-create table time_slot (
+create table if not exists time_slot (
 	time_slot_id varchar(4),
 	day          varchar(1),
 	start_hr     numeric(2)
@@ -115,11 +115,11 @@ create table time_slot (
 	primary key  (time_slot_id, day, start_hr, start_min)
 );
 
-create table prereq (
+create table if not exists prereq (
 	course_id   varchar(8),
 	prereq_id   varchar(8),
 	primary key (course_id, prereq_id),
-	foreign key (course_id) references course
+	foreign key (course_id) references course(course_id)
 		on delete cascade,
-	foreign key (prereq_id) references course
+	foreign key (prereq_id) references course(course_id)
 );

cd78224756225b8299db31361b1ee53a5a7f477e
Gaurav Juvekar <gauravjuvekar@gmail.com>
2016-09-10 22:03:24 +0530

    Uppercase SQL syntax

--- assignment_3/university/answers.sql
+++ assignment_3/university/answers.sql
@@ -1,20 +1,20 @@
-select name from student where tot_cred > 100;
-select course_id, grade from takes
-	where ID = (select ID from student where name = "Tanaka");
-select ID, name from instructor
-	where ID in
-		(select ID from teaches
-			where course_id in
-				(select course_id from course where dept_name = 'Comp. Sci.'));
-select title from course
-	where course_id in
-		(select course_id from section where semester in ('Fall', 'Spring'));
-select name from instructor where dept_name = 'Comp. Sci.';
+SELECT name FROM student WHERE tot_cred > 100;
+SELECT course_id, grade FROM takes
+	WHERE ID = (SELECT ID FROM student WHERE name = "Tanaka");
+SELECT ID, name FROM INstructor
+	WHERE ID IN
+		(SELECT ID FROM teaches
+			WHERE course_id IN
+				(SELECT course_id FROM course WHERE dept_name = 'Comp. Sci.'));
+SELECT title FROM course
+	WHERE course_id IN
+		(SELECT course_id FROM section WHERE semester IN ('Fall', 'SprINg'));
+SELECT name FROM INstructor WHERE dept_name = 'Comp. Sci.';
 
-select course_id, title from course
-	where course_id in
-		(select course_id from teaches
-			where ID = (select ID from instructor where name = 'Srinivasan'));
-select name from instructor
-	where ID in
-		(select ID from teaches where semester = 'Spring' and year = 2009);
+SELECT course_id, title FROM course
+	WHERE course_id IN
+		(SELECT course_id FROM teaches
+			WHERE ID = (SELECT ID FROM INstructor WHERE name = 'SrINivasan'));
+SELECT name FROM INstructor
+	WHERE ID IN
+		(SELECT ID FROM teaches WHERE semester = 'SprINg' AND year = 2009);
